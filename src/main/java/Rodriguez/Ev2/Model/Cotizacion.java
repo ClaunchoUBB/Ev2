0a4f51fd87c89e373330b0ddac44fe7bc2195f4a
Claudio Rodríguez Parra <claudio.rodriguez2201@alumnos.ubiobio.cl>
2025-12-30 18:06:10 -0300

    Se implementó la clase cotizacion

--- src/main/java/Rodriguez/Ev2/Model/Cotizacion.java
+++ src/main/java/Rodriguez/Ev2/Model/Cotizacion.java
@@ -1,12 +1,37 @@
 package Rodriguez.Ev2.Model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table (name = "cotizacion")
 public class Cotizacion {
 
+    @Id
+    @GeneratedValue (strategy =  GenerationType.IDENTITY)
+    private int idCotizacion;
 
-    
+    @Column(name = "total")
+    private int total;
+
+    public int getIdCotizacion() {
+        return idCotizacion;
+    }
+
+    public void setIdCotizacion(int idCotizacion) {
+        this.idCotizacion = idCotizacion;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+        
 }
