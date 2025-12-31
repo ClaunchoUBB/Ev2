8eb61a216caf5badaec17450506fcfe632c48d54
Claudio Rodríguez Parra <claudio.rodriguez2201@alumnos.ubiobio.cl>
2025-12-31 14:54:20 -0300

    Se comentó el .sql para inicializar la db

--- db/polloshermanos.sql
+++ db/polloshermanos.sql
@@ -23,6 +23,8 @@ CREATE TABLE mueble(
     material VARCHAR(30)
 );
 
+
+#Tabla variante que guarda información de las variantes
 CREATE TABLE variante(
     ID_variante INT PRIMARY KEY,
     nombre_variante VARCHAR(30),
@@ -30,31 +32,40 @@ CREATE TABLE variante(
     precio_extra INT
 );  
 
+
+#Tabla item, guarda un mueble y su variante
 CREATE TABLE item(
     ID_item INT PRIMARY KEY,
     ID_mueble INT NOT NULL,
     ID_cotizacion INT,
-    precio INT
+    precio INT,
+    cantidad INT
 );
 
 
+#Tabla para el muchos a muchos entre item y variante
 CREATE TABLE conexion_item_variante(
     ID_conexion_item_variante INT PRIMARY KEY,
     ID_item INT NOT NULL,
     ID_variante INT NOT NULL
 );
 
+
+#Tabla para guardar el total de una cotización
 CREATE TABLE cotizacion(
     ID_cotizacion INT PRIMARY KEY,
     total INT
 );
 
+
+#Tabla que guarda una venta Y su fecha
 CREATE TABLE venta(
     ID_venta INT PRIMARY KEY,
-    ID_cotizacion INT NOT NULL
+    ID_cotizacion INT NOT NULL,
+    fecha TIMESTAMP
 );
 
-
+#Aquí se añaden las constraints para las foreign keys
 ALTER TABLE item
     ADD FOREIGN KEY (ID_mueble) REFERENCES mueble(ID_mueble),
     ADD FOREIGN KEY (ID_cotizacion) REFERENCES cotizacion(ID_cotizacion);
