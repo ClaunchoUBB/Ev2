89284e17d02c97f16a5c335ed8a6593bc5204a59
Claudio Rodríguez Parra <claudio.rodriguez2201@alumnos.ubiobio.cl>
2026-01-01 19:02:42 -0300

    Testeo del CRUD de mueble funcional

--- src/test/java/Rodriguez/Ev2/MuebleTest.java
+++ src/test/java/Rodriguez/Ev2/MuebleTest.java
@@ -1,6 +1,7 @@
 package Rodriguez.Ev2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,11 @@ import jakarta.transaction.Transactional;
 @SpringBootTest
 @Transactional
 public class MuebleTest {
-    
+
     @Autowired
     private MuebleService servicioMueble;
 
-    public static Mueble buildMueble(){
+    public static Mueble buildMueble() {
         Mueble muebleCreado = new Mueble();
         muebleCreado.setEstado(true);
         muebleCreado.setMaterial("Madera");
@@ -29,10 +30,9 @@ public class MuebleTest {
         return muebleCreado;
     }
 
-
-    //El primer test sirve para comprobar si el mueble se guarda en la DB
+    // El primer test sirve para comprobar si el mueble se guarda en la DB
     @Test
-    public void saveTest(){
+    public void testCreateMueble() {
         Mueble muebleTesteo = buildMueble();
 
         servicioMueble.saveMueble(muebleTesteo);
@@ -42,8 +42,40 @@ public class MuebleTest {
         assertEquals(muebleTesteo, muebleEnDatabase);
     }
 
+    @Test
+    public void testDeleteMueble() {
 
-    
+        Mueble muebleTesteo = buildMueble();
 
+        servicioMueble.saveMueble(muebleTesteo);
+
+        servicioMueble.deleteMueble(muebleTesteo.getIdMueble());
+
+        assertNull(servicioMueble.getMueble(muebleTesteo.getIdMueble()));
+
+    }
+
+    @Test
+    public void testUpdateMueble() {
+
+        Mueble muebleTesteo = buildMueble();
+
+        servicioMueble.saveMueble(muebleTesteo);
+
+        Mueble otroMueble = buildMueble();
+
+        otroMueble.setMaterial("Plata");
+        otroMueble.setNombreMueble("Silla de plata");
+        otroMueble.setPrecioBase(80000);
+        otroMueble.setStock(80);
+
+        servicioMueble.updateMueble(muebleTesteo.getIdMueble(), otroMueble);
+
+        assertEquals(servicioMueble.getMueble(muebleTesteo.getIdMueble()).getNombreMueble(), "Silla de plata");
+        assertEquals(servicioMueble.getMueble(muebleTesteo.getIdMueble()).getStock(), 80);
+        assertEquals(servicioMueble.getMueble(muebleTesteo.getIdMueble()).getPrecioBase(), 80000);
+        assertEquals(servicioMueble.getMueble(muebleTesteo.getIdMueble()).getMaterial().toLowerCase(),"plata");
+
+    }
 
 }
