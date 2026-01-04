c0b31a5760e828dc4d9ad62ee74f7a3faed2a7e3
Claudio Rodríguez Parra <claudio.rodriguez2201@alumnos.ubiobio.cl>
2026-01-03 23:43:54 -0300

    Se añadió la colección en Bruno para que todo tenga visibilidad en el repositorio

---
+++ src/main/java/Rodriguez/Ev2/Controllers/ItemController.java
@@ -0,0 +1,10 @@
+package Rodriguez.Ev2.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ev2/items")
+public class ItemController {
+    
+}
