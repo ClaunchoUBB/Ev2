ba6e95d9c891018716394270ea9d6b94f3c1760c
Claudio Rodríguez Parra <claudio.rodriguez2201@alumnos.ubiobio.cl>
2026-01-03 23:17:32 -0300

    Añadí metodos reciclados de la evaluación 3

--- src/main/java/Rodriguez/Ev2/Controllers/MuebleController.java
+++ src/main/java/Rodriguez/Ev2/Controllers/MuebleController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.service.annotation.GetExchange;
@@ -29,5 +33,22 @@ public class MuebleController {
     public ResponseEntity<Mueble> getById(@PathVariable int id){
         Mueble m = servicioMueble.getMueble(id);
         return (m!=null) ? ResponseEntity.ok(m) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Mueble> create(@RequestBody Mueble mueble) {
+        return ResponseEntity.ok(servicioMueble.saveMueble(mueble));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Mueble> update(@PathVariable int id, @RequestBody Mueble reemplazo) {
+        Mueble updated = servicioMueble.updateMueble(id, reemplazo);
+        return (updated != null) ? ResponseEntity.ok(updated) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        servicioMueble.deleteMueble(id);
+        return ResponseEntity.noContent().build();
     }
 }
